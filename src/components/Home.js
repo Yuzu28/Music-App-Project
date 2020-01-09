4dfab10ddb8f4c429fb8140c765a38e12af724b1
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-01-08 21:23:18 -0500

    styling the homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,30 +12,17 @@ import './Home.css';
 
 
             <div id="root">
-                <div class="container-fluid">
+                <div className="container-fluid">
                       <div className="card" >
                         <div className="card-body">
-                            <h5 className="card-title">Welcome to Synchro-9</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title"><u>Welcome to <span className="titless">Synchro-9</span></u></h5>
+                            <p className="card-text"> Search for your Favorite Music, Lyrics, and Videos.</p>
                             
                         </div>
                     </div>
                 </div>
             </div>
 
-                // {/* <h1>ddd</h1> */}
-
-                //   {/* <div className="bb " >
-
-                //     <div className="info-box bird" id="sub">
-                //         <h4>Search for Any Games</h4>
-                //         <p>Users can search through a large variety of games in our database</p>
-                //         <br></br>
-                //         </div>
-                //     </div> */}
-
-
-                  
 
                     
 
