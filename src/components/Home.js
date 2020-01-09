6c3b77533a6c58401048379b0c4879a06bf90289
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-01-08 19:42:12 -0500

    add style on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-// import './Home.css';
+import './Home.css';
 // import {Link} from 'react-router-dom';
 
 
@@ -8,7 +8,43 @@ import React, {Component} from 'react';
 
     render(){
         return(
-            <h1>Homepahe</h1>
+            // <h1>Homepahe</h1>
+
+
+            <div id="root">
+                <div class="container-fluid">
+                      <div className="card" >
+                        <div className="card-body">
+                            <h5 className="card-title">Welcome to Synchro-9</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                // {/* <h1>ddd</h1> */}
+
+                //   {/* <div className="bb " >
+
+                //     <div className="info-box bird" id="sub">
+                //         <h4>Search for Any Games</h4>
+                //         <p>Users can search through a large variety of games in our database</p>
+                //         <br></br>
+                //         </div>
+                //     </div> */}
+
+
+                  
+
+                    
+
+
+
+
+
+
+            
         )
     }
 }
--- src/components/MusicSearch.js
+++ src/components/MusicSearch.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-// import './Home.css';
+import './MusicSearch.css';
 // import {Link} from 'react-router-dom';
 // import Navbar from './components/Navbar';
 
