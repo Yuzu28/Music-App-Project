83c244f7e859f84de3134ebefea056a99c5086e3
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-01-10 00:47:11 -0500

    got the music api working

--- src/components/MusicSearch.js
+++ src/components/MusicSearch.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './MusicSearch.css';
-// import {Link} from 'react-router-dom';
-// import Navbar from './components/Navbar';
+import axios from 'axios';
 
 
 
@@ -9,24 +8,62 @@ import './MusicSearch.css';
 
     state ={
 
-        artistQuery: ''
+        artistQuery: '',
+        artist: null
     }
 
 
-    //below fires the moment when the value of the element is changed
+    //below fires the moment when the value of the element is changed when you type something in the search bar
     updateArtistQuery = (path) =>{
-        console.log('path.target.value', path.target.value);
+        // console.log('path.target.value', path.target.value);
 
         //update the component
+
+        // setting the state when searching an artist
         this.setState({ artistQuery: path.target.value });
     }
 
     searchArtist = (e) =>{
         e.preventDefault();
 
-        console.log('this.state', this.state);
+        //grading the searchItem from the state already set 
+        const musicTile = this.state.artistQuery;
+        // console.log(musicTile)
+
+        //log checking the current state
+        // console.log('this.state', this.state);
+
+        const api_call = `https://spotify-api-wrapper.appspot.com/artist/${musicTile}`;
+        
+
+        axios.get(api_call).then((response)=>{
+            console.log(response.data);
+            console.log(response.data.artists.total);
+
+            if (response.data.artists.total > 0){
+                const artist = response.data.artists.items[0];
+
+                this.setState({artist})
+
+                //geting the top track for the users after passing the if statment. 
+                const api_call_top_tracks = `https://spotify-api-wrapper.appspot.com/artist/${artist.id}/top-tracks`;
+                // console.log(api_call_top_tracks);
+
+
+                axios.get(api_call_top_tracks ).then((response)=>{
+                    console.log(response.data);})
+
+                    
+
+                
+
+            }
        
-    }
+    })
+}
+
+  
+    
 
     render(){
         return(
@@ -53,3 +90,29 @@ import './MusicSearch.css';
 }
 
 export default MusicSearch;
+
+
+
+//not working
+// Client ID e679215f259840b1aa8ce8e8de5a5da0
+
+// var proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+//         // axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
+//         const api_call = {
+            
+//             url: proxyUrl + `https://api.spotify.com/v1/search?q=${musicTile}&type=track%2C%20artist;`,
+//             method: 'POST',
+//             headers = {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json',
+//                 'Authorization': 'Bearer e679215f259840b1aa8ce8e8de5a5da0',
+//             }
+//         }
+
+
+
+      
+
+    
