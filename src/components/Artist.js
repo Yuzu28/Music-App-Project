f454aa51ee1e91fb5d62cafd07e6902f2ca2a2ca
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-01-10 23:07:14 -0500

    able to search an artist and post

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -3,6 +3,10 @@ import React from 'react';
 const Artist = ({artist}) => {
 
     if (!artist) return null;
+   
+
+    else {
+
 
     const{ images, name, followers, genres} = artist;
 
@@ -11,10 +15,25 @@ const Artist = ({artist}) => {
             <h3>{name}</h3>
             <p className='info'>{followers.total} followers</p>
             <p className='info' >{genres.join(', ')}</p>
-            <img className="artistRes" src={images[0].url} alt='artist profile picture' height="200" width="200" borderRadius="100" />
+
+            {/* fix problems when some of the content dont have images */}
+             {
+                 images[0]? 
+                    <img className="artistRes"  src={images[0].url} alt='artist profile picture' height="200" width="200" />
+                    : 
+                    <img className="artistRes"  src={process.env.PUBLIC_URL + '/noimage.jpg'} height="60%" width="90%" alt="img"   />
+             }
+
+
+
+
+
+
+            {/* <img className="artistRes" src={images[0].url} alt='artist profile picture' height="200" width="200" /> */}
         </div>
     )
 
+}
 }
 
 export default Artist;
