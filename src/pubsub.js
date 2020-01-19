2aa1cea945bdcbf6fe2751b228e5120ed7f884ca
John Nguyen <JohnNguyen11@Johns-MacBook-Pro-6.local>
2020-01-18 19:15:14 -0500

    redux connections

--- src/pubsub.js
+++ src/pubsub.js
@@ -1,22 +1,41 @@
 
 import PubNub from 'pubnub';
 import pubnubConfig from './pubnub.config';
+// import { listenerCount } from 'cluster';
  
-const pubnub = new PubNub(pubnubConfig);
+// const pubnub = new PubNub(pubnubConfig);
  
 export const MESSAGE_CHANNEL = 'MESSAGE_CHANNEL';
- 
-pubnub.subscribe({ channels: [MESSAGE_CHANNEL] });
-pubnub.addListener({
-    message: (messageObject) => {
-        console.log('messageObject', messageObject);
+
+class PubSub {
+    constructor(){
+        this.pubnub = new PubNub(pubnubConfig);
+
+        this.pubnub.subscribe({ channels: [MESSAGE_CHANNEL] });
     }
-});
+    addListener = listenerConfig => {
+        this.pubnub.addListener(listenerConfig);
+    }
+
+    publish = message => {
+        console.log('publish messgae', message)
+        this.pubnub.publish({ message, channel: MESSAGE_CHANNEL })
+    }
+}
+export default PubSub;
+
+ 
+// pubnub.subscribe({ channels: [MESSAGE_CHANNEL] });
+// pubnub.addListener({
+//     message: (messageObject) => {
+//         console.log('messageObject', messageObject);
+//     }
+// });
  
  
-setTimeout(() => {
-    pubnub.publish({
-        message: 'foo',
-        channel: MESSAGE_CHANNEL
-    });
-}, 1000)
+// setTimeout(() => {
+//     pubnub.publish({
+//         message: 'foo',
+//         channel: MESSAGE_CHANNEL
+//     });
+// }, 1000)
